f2ab692d9af110a9f2b8c1abb5d7bfa117c3b931
sathsubramanian <you@example.com>
2018-06-06 16:10:42 +0530

    add baspackage path

--- spring-javaj2eefsd-workshop/src/main/java/io/swagger/Swagger2SpringBoot.java
+++ spring-javaj2eefsd-workshop/src/main/java/io/swagger/Swagger2SpringBoot.java
@@ -5,22 +5,22 @@ import org.springframework.boot.ExitCodeGenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 
 @SpringBootApplication
 @EnableSwagger2
-@ComponentScan(basePackages = { "io.swagger", "com.javaj2eefsd.workshop.api" })
+@ComponentScan(basePackages = { "io.swagger", "com.javaj2eefsd.workshop" })
 public class Swagger2SpringBoot implements CommandLineRunner {
 
     @Override
-    public void run(String... arg0) throws Exception {
+    public void run(final String... arg0) throws Exception {
         if (arg0.length > 0 && arg0[0].equals("exitcode")) {
             throw new ExitException();
         }
     }
 
-    public static void main(String[] args) throws Exception {
+    public static void main(final String[] args) throws Exception {
         new SpringApplication(Swagger2SpringBoot.class).run(args);
     }
 
