4f45690cd32dcba0af6b762d290e6723ffcbb802
sathsubramanian <you@example.com>
2018-06-29 20:40:40 +0530

    create costant message class

---
+++ spring-javaj2eefsd-workshop/src/main/java/com/javaj2eefsd/workshop/constant/ErrorMassage.java
@@ -0,0 +1,12 @@
+/**
+ *
+ */
+package com.javaj2eefsd.workshop.constant;
+
+/**
+ * @author sathsubramanian
+ */
+public class ErrorMassage {
+    public static final String INVALIDEXPENSEID = "expense Id is not correct";
+    public static final String SEARCHKEYISNULL = "search key is null";
+}
