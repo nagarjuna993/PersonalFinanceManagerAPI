6a2778a0c551c52c84bb8bca1dbd32a88a6b7fac
Nagarjuna <nagarjuna993@hotmail.com>
2018-08-07 12:20:36 +0530

    Added encryption utility and implemented in register

--- spring-javaj2eefsd-workshop/src/main/java/com/javaj2eefsd/workshop/dao/RegisterDaoImpl.java
+++ spring-javaj2eefsd-workshop/src/main/java/com/javaj2eefsd/workshop/dao/RegisterDaoImpl.java
@@ -1,6 +1,16 @@
 package com.javaj2eefsd.workshop.dao;
 
 import java.util.List;
+import java.util.Properties;
+import java.util.Random;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,8 +21,8 @@ import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Repository;
 
-import com.javaj2eefsd.workshop.model.Investments;
 import com.javaj2eefsd.workshop.model.User;
+import com.javaj2eefsd.workshop.util.EncryptionUtil;
 
 @Repository
 public class RegisterDaoImpl implements RegisterDao {
@@ -26,11 +36,16 @@ public class RegisterDaoImpl implements RegisterDao {
 	@Override
 	public User registerUser(User registeruser) throws Exception  {
 		// TODO Auto-generated method stub
+		EncryptionUtil encObj = new EncryptionUtil();
+		int otp = generateOTP();
 		try {
+			
+			registeruser.setPassword(encObj.encrypt(registeruser.getPassword().trim())[0]);
 			//Setting user status to False
-			//TODO: Set user status to true after impl
-			registeruser.setUserStatus(true);
+			registeruser.setUserStatus(false);
+			registeruser.setOtp(otp);
             mongoTemplate.save(registeruser);
+          //this.sendEmail(registeruser.getEmailId(),registeruser.getOtp());
         }
         catch (final Exception e) {
             throw new Exception(e.getMessage());
@@ -75,6 +90,50 @@ try {
         }
 		
 
+	}
+	
+	public void sendEmail(String emailId, Integer otp) throws Exception {
+		// TODO Auto-generated method stub
+		
+		final String username = "dakshinn@gmail.com";
+		final String password = "";
+
+		Properties props = new Properties();
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.port", "587");
+
+		Session session = Session.getInstance(props,
+				new javax.mail.Authenticator() {
+					protected PasswordAuthentication getPasswordAuthentication() {
+						return new PasswordAuthentication(username, password);
+					}
+				});
+
+		try {
+
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress("from-email@gmail.com"));
+			message.setRecipients(Message.RecipientType.TO,
+					InternetAddress.parse(emailId));
+			message.setSubject("Testing Subject");
+			message.setText("Dear User"
+					+ "\n\n Welcome to PFM!");
+
+			Transport.send(message);
+
+			System.out.println("Done");
+
+		} catch (MessagingException e) {
+			throw new RuntimeException(e);
+		}
+		
+	}
+	
+	public int generateOTP() {
+		Random r = new Random();
+	    return r.nextInt(999999);
 	}
 		
 
