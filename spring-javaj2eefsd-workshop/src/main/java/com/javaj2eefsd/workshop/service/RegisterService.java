c21d328e0d20f3905c6274af9c422d41bcb30656
dakshinn <36534887+dakshinn@users.noreply.github.com>
2018-08-08 12:43:01 +0530

    Update RegisterService.java

--- spring-javaj2eefsd-workshop/src/main/java/com/javaj2eefsd/workshop/service/RegisterService.java
+++ spring-javaj2eefsd-workshop/src/main/java/com/javaj2eefsd/workshop/service/RegisterService.java
@@ -5,7 +5,7 @@ import com.javaj2eefsd.workshop.model.User;
 public interface RegisterService {
 	
 	public User registerUser(User registeruser) throws Exception;
-	public void activateUser(String userId, Integer otp) throws Exception;
+	public void activateUser(String emailId, Integer otp) throws Exception;
 	public void sendEmail(String emailId, Integer otp) throws Exception;
 
 }
